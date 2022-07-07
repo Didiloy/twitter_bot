ec1f7c2ff77be7e877590ed76a8110352287d81a
Didiloy <didiloy2001@gmail.com>
2022-07-07 13:43:57 +0200

    dialog with the twitter api

--- main.js
+++ main.js
@@ -6,8 +6,5 @@ import { rwClient } from './twitter.js';
 let blague = await GetRandomBlague().then((b) => {
   return b;
 });
-console.log(blague);
-// Instantiate with desired auth type (here's Bearer v2 auth)
-// const twitterClient = new TwitterApi(process.env.TWITTER_BEARER8TOKEN);
 
 await rwClient.v2.tweet(`${blague.joke}\n${blague.answer}`);
